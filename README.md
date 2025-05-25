cee7c2f0d47763121e1b02f4a5315aa06432a7bd
somnionocte <somnionocte@proton.me>
2025-05-25 15:22:15 +0300

    UPDATE

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 Overscroll is a simple API for creating spring effect on scroll and gestures on overscroll for your Jetpack Compose app.
 
+Example of use:
+```Kotlin
+val scrollState = rememberScrollState()
+
+Column(
+    Modifier
+        .fillMaxSize()
+        .bouncedOverscroll(scrollState)
+        .verticalScroll(scrollState)
+        .safeContentPadding()
+) {
+    repeat(100) {
+        Text("Some text $it")
+    }
+}
+```
+
 ### `bouncedOverscroll`
 
 A modifier that applies nestedScrollConnection and binds its overscroll animation to graphicsLayer for offset.
@@ -10,6 +27,17 @@ If you need to apply your own overscroll animation use delegateOverscroll.
 ### `delegateOverscroll`
 
 Modifier that uses nestedScrollConnection under the hood and uses the passed animatable as the overscroll position state.
+
+``` Kotlin
+fun Modifier.bouncedOverscroll(
+    mainScroll: ScrollableState,
+    overscrollOffset: Animatable<Float, AnimationVector1D>? = null,
+    orientation: Orientation = Orientation.Vertical,
+    gestureThreshold: Int? = null,
+    onGestureUp: () -> Unit = {  },
+    onGestureDown: () -> Unit = {  }
+)
+```
 
 In addition to the iOS scrolling effect, this functionality has more practical uses, such as smoothly closing a modal window using scrolling.
 
--- app/src/main/java/com/somnio_nocte/overscroll/MainActivity.kt
+++ app/src/main/java/com/somnio_nocte/overscroll/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,18 +17,12 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -39,8 +31,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
 import com.somnio_nocte.overscroll.ui.theme.OverscrollTheme
